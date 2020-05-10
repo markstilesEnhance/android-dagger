4e32127a7b18721e971a8498d31addc4e059adaa
markstilesEnhance <mark.stiles@enhanceit.us>
2020-05-10 13:29:49 -0400

    Task 15

--- app/src/main/java/com/example/android/dagger/di/StorageModule.kt
+++ app/src/main/java/com/example/android/dagger/di/StorageModule.kt
@@ -1,12 +1,32 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.storage.SharedPreferencesStorage
 import com.example.android.dagger.storage.Storage
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class RegistrationStorage
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class LoginStorage
 
 @Module
 abstract class StorageModule {
-    @Binds
-    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+
+    @RegistrationStorage
+    @Provides
+    fun provideRegistrationStorage(context: Context): Storage {
+        return SharedPreferencesStorage("registration", context)
+    }
+
+    @LoginStorage
+    @Provides
+    fun provideLoginStorage(context: Context): Storage {
+        return SharedPreferencesStorage("login", context)
+    }
 }
